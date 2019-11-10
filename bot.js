3abab0d52fbd50fc949961ff7e4e34bfc87c7a58
Zhao Yun <zynushigh@gmail.com>
2019-11-10 19:34:23 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,8 @@ const express = require('express')
 var port = process.env.PORT || 3000;
 const prefix = process.env.PREFIX;
 
+const PING_EMBED = new Discord.RichEmbed().setTitle("機器雲的延時").setColor(0x21f8ff).addField("Latency", 0).addField("Discord API Latency", 0);
+
 console.log('APP STARTING...');
 
 client.on('ready', () => {
@@ -26,6 +28,15 @@ client.on('message', async msg => {
     const args = msg.content.slice(1).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
     if (msg.channel.id === "642988383626592286" || msg.channel.id === "642993162943725568") {
+        if (command === "ping") {
+            msg.delete();
+            console.log(`${msg.author.username} requested a ping!`);
+            const m = await msg.channel.send("Ping?");
+            PING_EMBED.fields[0].value = m.createdTimestamp - msg.createdTimestamp;
+            PING_EMBED.fields[1].value = Math.round(client.ping);
+            PING_EMBED.setFooter("Requested By " + msg.author.username, msg.author.displayAvatarURL);
+            m.edit(PING_EMBED);
+        }
         if (command === "test") {
             msg.channel.send("TESTING COMMAND.  Arguments: " + args.join(", "));
             console.log(`${msg.author.username} requested test!`);
