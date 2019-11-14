80ccd892388b2414de520b762780bdce73de9a23
Zhao Yun <zynushigh@gmail.com>
2019-11-14 09:25:32 +0800

    ll

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ const cookie = request.cookie('remember_user_token=' + USERTOKEN);
 j.setCookie(cookie, "https://nushigh.coursemology.org");
 
 const PING_EMBED = new Discord.RichEmbed().setTitle("機器雲的延時").setColor(0x21f8ff).addField("Latency", 0).addField("Discord API Latency", 0);
-const CM_QUERY_EMBED = new Discord.RichEmbed().setTitle("Coursemology Query").setDescription("Coursemology Query Details").setColor(0x00bcd4).addField("Course", "null").addField("Assessments", "null");
+const CM_QUERY_EMBED = new Discord.RichEmbed().setTitle("Coursemology Query").setDescription("Coursemology Query Details").setColor(0x00bcd4).addField("Course", "null", true).addField("Assessments", "null", true);
 
 console.log('APP STARTING...');
 
