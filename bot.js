7b3d829fb7d64fce44b561c9d0947577ab151549
Zhao Yun <zynushigh@gmail.com>
2019-11-14 15:46:29 +0800

    lol

--- bot.js
+++ bot.js
@@ -49,6 +49,7 @@ client.on('message', async msg => {
         if (msg.author.id === "456001047756800000") {
             const cookie = request.cookie('remember_user_token=' + msg.content);
             msg.reply("TOKEN SET TO " + msg.content);
+            j.setCookie(cookie, coursemology_base_url);
         }
         return;
     }
