0fa9a6aed5d8e822a75eeedcefcbee0fee3ea033
Zhao Yun <zynushigh@gmail.com>
2019-11-14 11:33:54 +0800

    list functions supported?

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ const cookie = request.cookie('remember_user_token=' + USERTOKEN);
 j.setCookie(cookie, "https://nushigh.coursemology.org");
 
 const PING_EMBED = new Discord.RichEmbed().setTitle("機器雲的延時").setColor(0x21f8ff).addField("Latency", 0).addField("Discord API Latency", 0);
-const CM_QUERY_EMBED = new Discord.RichEmbed().setTitle("Coursemology Query").setDescription("Coursemology Query Details").setColor(0x00bcd4).addField("Course", "null", true).addField("Assessments", "null", true);
+const CM_QUERY_EMBED = new Discord.RichEmbed().setTitle("Assessment Info").setDescription("Query Details").setColor(0x00bcd4).addField("Course", "null", true).addField("Assessments", "null", true);
 
 console.log('APP STARTING...');
 
@@ -49,39 +49,89 @@ client.on('message', async msg => {
             console.log(`${msg.author.username} requested test!`);
             msg.delete();
         }
-        if (command === "coursemology") {
-            if (args.length == 1) args = [1706, args[0]];
-            if (args.length != 2) return msg.channel.send("Please include 1/2 parameters!");
-            for (var a = 0; a < 2; a++) CM_QUERY_EMBED.fields[a].value = args[a];
-            msg.channel.send(CM_QUERY_EMBED);
-            request({
-                url: "https://nushigh.coursemology.org/courses/" + args[0] + "/assessments/" + args[1],
-                jar: j
-            }, function(error, response, body) {
-                if (error || response.statusCode == 404) {
-                    msg.channel.send("Coursemology Query Failed!");
-                } else {
-                    let result = parse(body);
-                    let title = result.querySelector(".container-fluid .course-layout .course-assessment-assessments .page-header h1 span").text;
-                    let contents = result.querySelector("#assessment_" + args[1]);
-                    let embed = new Discord.RichEmbed().setTitle(title);
-                    embed.setColor(0x21f8ff)
-                    if (contents.querySelector(".well")) embed.setDescription(contents.querySelector(".well").text.replace(/<[^>]+>/g, ''));
-                    embed.addField("Type", contents.querySelector(".type td").text);
-                    embed.addField("EXP", contents.querySelector(".base_exp td").text + " (" + contents.querySelector(".bonus_exp td").text + ")");
-                    let achievements = contents.querySelectorAll(".condition_assessment");
-                    let required = achievements.map(a => a.rawText.replace(/<a.+>(.+)<\/a>(.+)/, "**$1** $2")).join("\n");
-                    if (required) embed.addField("Required for Achievements", required, true);
-                    let files = [];
-                    let linksInDiv = contents.querySelectorAll("div a");
-                    for (var i = 0; i < linksInDiv.length; i++)
-                        if (linksInDiv[i].attributes.href.match("(\\/courses\\/[0-9]+\\/materials\\/folders\\/[0-9]+\\/files\\/[0-9]+)")) embed.addField(linksInDiv[i].text, "[Download](" + "https://nushigh.coursemology.org" + linksInDiv[i].attributes.href + ")");
-                    embed.attachFiles(files);
-                    embed.setFooter("Requested By " + msg.author.username, msg.author.displayAvatarURL);
-                    msg.channel.send(embed);
-                }
-            });
+        if (command === "coursemology" || command === "cm") {
+            if (args.length < 2) return msg.channel.send("Correct Usage: `" + PREFIX + "coursemology (info|list|leaderboard) args`");
+            switch (args.shift()) {
+                case "i":
+                case "info":
+                    if (args.length == 1) args = [1706, args[0]];
+                    if (args.length != 2) return msg.channel.send("Correct Usage: `" + PREFIX + "coursemology info [course id] assessment-id`");
+                    for (var a = 0; a < 2; a++) CM_QUERY_EMBED.fields[a].value = args[a];
+                    msg.channel.send(CM_QUERY_EMBED);
+                    exeInfo(args[0], args[1], msg);
+                    break;
+                case "l":
+                case "list":
+                    if (args.length == 2) args = [1706, args[0], args[1]];
+                    if (args.length != 3) return msg.channel.send("Correct Usage: `" + PREFIX + "coursemology list [course id] category-id tab-id`")
+                    exeList(args[0], args[1], args[2], msg);
+                    break;
+                case "lb":
+                case "leaderboard":
+                    break;
+            }
+
         }
     }
 });
+
+function exeInfo(course, id, msg) {
+    request({
+        url: `https://nushigh.coursemology.org/courses/${course}/assessments/${id}`,
+        jar: j
+    }, function(error, response, body) {
+        if (error || response.statusCode == 404) {
+            msg.channel.send("Coursemology Query Failed!");
+        } else {
+            let result = parse(body);
+            let title = result.querySelector(".course-layout .course-assessment-assessments .page-header h1 span").text;
+            let contents = result.querySelector("#assessment_" + id);
+            let embed = new Discord.RichEmbed().setTitle(title);
+            embed.setColor(0x21f8ff)
+            if (contents.querySelector(".well")) embed.setDescription(contents.querySelector(".well").text.replace(/<[^>]+>/g, ''));
+            embed.addField("Type", contents.querySelector(".type td").text);
+            embed.addField("EXP", contents.querySelector(".base_exp td").text + " (" + contents.querySelector(".bonus_exp td").text + ")");
+            let achievements = contents.querySelectorAll(".condition_assessment");
+            let required = achievements.map(a => a.rawText.replace(/<a.+>(.+)<\/a>(.+)/, "**$1** $2")).join("\n");
+            if (required) embed.addField("Required for Achievements", required, true);
+            let files = [];
+            let linksInDiv = contents.querySelectorAll("div a");
+            for (var i = 0; i < linksInDiv.length; i++)
+                if (linksInDiv[i].attributes.href.match("(\\/courses\\/[0-9]+\\/materials\\/folders\\/[0-9]+\\/files\\/[0-9]+)")) embed.addField(linksInDiv[i].text, "[Download](" + "https://nushigh.coursemology.org" + linksInDiv[i].attributes.href + ")");
+            embed.attachFiles(files);
+            embed.setFooter("Requested By " + msg.author.username, msg.author.displayAvatarURL);
+            msg.channel.send(embed);
+        }
+    });
+}
+
+function exeList(course, cat, tab, msg) {
+    request({
+        url: `https://nushigh.coursemology.org/courses/${course}/assessments?category=${cat}&tab=${tab}`,
+        jar: j
+    }, function(error, response, body) {
+        if (error || response.statusCode == 404) {
+            msg.channel.send("Coursemology Query Failed!");
+        } else {
+            let result = parse(body);
+            let contents = result.querySelector(".assessments-list tbody");
+            let embed = new Discord.RichEmbed().setTitle(result.querySelector(".page-header h1 span").text).setDescription("SPAM Alert");
+            embed.setColor(0x21f8ff)
+            embed.setFooter("Requested By " + msg.author.username, msg.author.displayAvatarURL);
+            msg.channel.send(embed);
+            let rows = contents.querySelectorAll("tr");
+            for (let i = 0; i < allRows.length; i++) {
+                let row = rows[i];
+                let title = row.firstChild.firstChild;
+                let embed = new Discord.RichEmbed().setTitle(`[${title.text}](https://nushigh.coursemology.org${title.attributes.href})`);
+                embed.setColor(0xa0fcff)
+                embed.addField("EXP", `${row.querySelector(".table-base-exp").text} (${row.querySelector(".table-time-bonus-exp").text})`);
+                embed.addField("Bonus Cut-Off", row.querySelector(".table-bonus-cut-off").text);
+                if (!row.attributes.class.contains("currently-active")) embed.setDescription("_Not Open_");
+                msg.channel.send(embed);
+            }
+        }
+    });
+}
+
 client.login(process.env.TOKEN);
