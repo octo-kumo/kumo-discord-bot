42a88701cb7ba4024cb3cfa407c0ada40b149d50
Zhao Yun <zynushigh@gmail.com>
2019-11-16 08:42:11 +0800

    oof

--- bot.js
+++ bot.js
@@ -103,14 +103,14 @@ client.on('message', async msg => {
                     exeLU(1706, msg.channel, msg.author);
                 } else if (args.length == 1) {
                     console.log("user subcommand, only user provided, proceed to stalk that user...");
-                    exeStalk(1706, args[0]);
+                    exeStalk(1706, args[0], msg.channel, msg.author);
                 } else if (args.length == 2) {
                     if (args[0] === "list") {
                         console.log("user subcommand, course provided, requested list, proceed to list all users...");
                         exeLU(args[1], msg.channel, msg.author);
                     } else {
                         console.log("user subcommand, all args provided, proceed to stalk that user...");
-                        exeStalk(args[0], args[1]);
+                        exeStalk(args[0], args[1], msg.channel, msg.author);
                     }
                 } else {
                     console.log("user subcommand, invalid args, proceed to warn the user...");
