495b844f698d544edd118d83c997360a5d45b4c0
Zhao Yun <zynushigh@gmail.com>
2019-11-16 20:10:41 +0800

    lol error #1

--- config.js
+++ config.js
@@ -16,5 +16,5 @@ exports.config = {
     NUMBER_OF_USER_PER_PAGE: 8,
     COURSES: [1706, 1389, 614, 1613],
     SLEEP_IMAGES: ["https://res.cloudinary.com/chatboxzy/image/upload/v1573747146/sleep_1.jpg", "https://res.cloudinary.com/chatboxzy/image/upload/v1573747147/sleep_2.jpg", "https://res.cloudinary.com/chatboxzy/image/upload/v1573747147/sleep_3.jpg", "https://res.cloudinary.com/chatboxzy/image/upload/v1573747147/sleep_4.jpg"],
-    SLEEP_LATE: ["https://res.cloudinary.com/chatboxzy/image/upload/v1573747132/sleep_late.jpg"];
+    SLEEP_LATE: ["https://res.cloudinary.com/chatboxzy/image/upload/v1573747132/sleep_late.jpg"]
 }
