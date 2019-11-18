82e60cc6b031979570b3b06dc4a49d6c5885b7d9
Zhao Yun <zynushigh@gmail.com>
2019-11-18 23:29:27 +0800

    level 120 yay

--- azurlane.js
+++ azurlane.js
@@ -11,7 +11,8 @@ const COLOR = {
     "Rare": 0xb0e0e6,
     "Elite": 0xdda0dd,
     "Super Rare": 0xeee8aa,
-    "Unreleased": 0x000000
+    "Unreleased": 0x000000,
+    "Decisive": 0xffffff
 };
 const DATA = {};
 
@@ -31,24 +32,25 @@ exports.handleCommnd = async function(args, msg, PREFIX) {
             try {
                 const ship = await azurlane.getShipByName(args.join(" "));
                 let embed = new Discord.RichEmbed().setTitle(`**${ship.names[lang]}**`).setColor(COLOR[ship.rarity]).setThumbnail(ship.thumbnail).setURL(ship.wikiUrl);
+                let stats = ship.stats.level120;
                 embed.addField("**ID**", ship.id, true)
                     .addField("**Stars**", ship.stars.value, true)
                     .addField("**Rarity**", ship.rarity, true)
                     .addField("**Type**", ship.hullType, true)
                     .addField("**Class**", ship.class, true)
                     .addField("**Nationality**", ship.nationality, true)
-                    .addField("Health", ship.stats.base[0].value, true)
-                    .addField("Armor", ship.stats.base[1].value, true)
-                    .addField("Reload", ship.stats.base[2].value, true)
-                    .addField("Luck", ship.stats.base[3].value, true)
-                    .addField("Firepower", ship.stats.base[4].value, true)
-                    .addField("Evasion", ship.stats.base[6].value, true)
-                    .addField("Anti-air", ship.stats.base[8].value, true)
-                    .addField("Aviation", ship.stats.base[9].value, true)
-                    .addField("Oil Usage", ship.stats.base[10].value, true)
+                    .addField("Health", stats[0].value, true)
+                    .addField("Armor", stats[1].value, true)
+                    .addField("Reload", stats[2].value, true)
+                    .addField("Luck", stats[3].value, true)
+                    .addField("Firepower", stats[4].value, true)
+                    .addField("Evasion", stats[6].value, true)
+                    .addField("Anti-air", stats[8].value, true)
+                    .addField("Aviation", stats[9].value, true)
+                    .addField("Oil Usage", stats[10].value, true)
                     .addField("Designed by", ship.miscellaneous.artist.name)
                     .addField("Avaliable Skins", ship.skins.map(skin => skin.title).join("\n"));
-                embed.setDescription("_All stats shown below are base stats._");
+                embed.setDescription("_All stats shown below are lv120 stats._");
                 msg.channel.send(embed);
             } catch (err) {
                 console.log(`ship subcommand, err code = ${err.statusCode}, args = ${args}`);
@@ -67,7 +69,7 @@ exports.handleCommnd = async function(args, msg, PREFIX) {
                 }
             } catch (err) {
                 console.log(`ships subcommand, err code = ${err.statusCode}, args = ${args}`);
-                msg.channel.send("Invalid ship name.");
+                msg.channel.send("Invalid ships category/type.");
             }
             break;
         case "viewskin":
