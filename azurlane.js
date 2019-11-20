e084a2ac97da79d9e9482808945a9207e741e3aa
Zhao Yun <zynushigh@gmail.com>
2019-11-20 15:15:10 +0800

    total overhual to azurlane, the author of the old api decided to make it paid

--- azurlane.js
+++ azurlane.js
@@ -1,11 +1,10 @@
-const {
-    AzurLane,
-    Category
-} = require("azurlane");
-const azurlane = new AzurLane();
+const request = require('request');
+const JSDOM = require('jsdom').JSDOM;
 const Discord = require('discord.js');
 const config = require('./config.js').config;
-
+const headers = {
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0"
+};
 const COLOR = {
     "Normal": 0xdcdcdc,
     "Rare": 0xb0e0e6,
@@ -15,8 +14,9 @@ const COLOR = {
     "Unreleased": 0x000000,
     "Decisive": 0xffffff
 };
-const DATA = {};
-
+const SHIPS = [];
+const SHIPS_CACHE = {};
+exports.ships = SHIPS;
 exports.handleCommnd = async function(args, msg, PREFIX) {
     console.log("running azurlane sub-system...");
     if (args.length < 1) return msg.channel.send("Correct Usage: `" + PREFIX + "azurlane (ship|skin) [args]`");
@@ -32,11 +32,11 @@ exports.handleCommnd = async function(args, msg, PREFIX) {
         case "i":
             try {
                 console.log("Getting Ship " + args.join(" "));
-                const ship = await azurlane.getShipByName(args.join(" "));
+                const ship = await getShipByName(args.join(" "));
                 let embed = new Discord.RichEmbed().setTitle(`**${ship.names[lang]}**`).setColor(COLOR[ship.rarity]).setThumbnail(ship.thumbnail).setURL(ship.wikiUrl);
                 let stats = ship.stats.level120;
                 embed.addField("**ID**", (ship.id) ? ship.id : "**not yet decided**", true)
-                    .addField("**Stars**", ship.stars.value, true)
+                    .addField("**Stars**", ship.stars, true)
                     .addField("**Rarity**", "**" + ship.rarity + "**", true)
                     .addField("**Type**", ship.hullType, true)
                     .addField("**Class**", ship.class, true)
@@ -70,7 +70,7 @@ exports.handleCommnd = async function(args, msg, PREFIX) {
             try {
                 let newArgs = args.join(" ").split(/ *\| */g);
                 if (newArgs.length == 1) newArgs = [newArgs[0], "Default"];
-                const ship = await azurlane.getShipByName(newArgs[0]);
+                const ship = await getShipByName(newArgs[0]);
                 let skin = ship.skins.filter(skin => skin.title.toUpperCase().includes(newArgs[1].toUpperCase()))[0];
                 let embed = new Discord.RichEmbed().setTitle(`**${ship.names[lang]}** (${skin.title})`).setColor(COLOR[ship.rarity]).setThumbnail(skin.chibi).setURL(ship.wikiUrl);
                 embed.addField("Avaliable Skins", ship.skins.map(lskin => lskin.title === skin.title ? "**" + lskin.title + "**" : lskin.title).join("\n"));
@@ -82,4 +82,91 @@ exports.handleCommnd = async function(args, msg, PREFIX) {
             }
             break;
     }
+}
+
+function findExactShip(name) {
+    return SHIPS.find(ship => ship.name.toUpperCase() === name.toUpperCase());
+}
+
+function findShip(name) {
+    return SHIPS.find(ship => ship.name.toUpperCase().includes(name.toUpperCase()));
+}
+
+function getShipByName(name) {
+    return new Promise((resolve, reject) => {
+        let cacheShip = findExactShip(name);
+        if (!cacheShip) cacheShip = findShip(name);
+        if (cacheShip) {
+            if (SHIPS_CACHE.hasOwnProperty(cacheShip.id)) {
+                console.log("Found it in cache. Serving Cache Content");
+                resolve(SHIPS_CACHE[cacheShip.id]);
+                return;
+            }
+            request({
+                url: "https://azurlane.koumakan.jp/" + cacheShip.name,
+                headers: headers
+            }, (error, res, body) => {
+                if (error) reject(error);
+                const doc = new JSDOM(body).window.document;
+                const arts = doc.querySelector("#Art tbody").getElementsByTagName("a");
+                const tabs = doc.querySelectorAll(".azl_box_body .tabber .tabbertab");
+                let ship = {
+                    wikiUrl: "https://azurlane.koumakan.jp/" + cacheShip.name,
+                    id: cacheShip.id,
+                    names: {
+                        en: cacheShip.name,
+                        cn: doc.querySelector('[lang="zh"]').textContent,
+                        jp: doc.querySelector('[lang="ja"]').textContent,
+                        kr: doc.querySelector('[lang="ko"]').textContent
+                    },
+                    thumbnail: "https://azurlane.koumakan.jp" + doc.querySelector("div:nth-child(1) > div:nth-child(2) > .image > img").getAttribute("src"),
+                    skins: Array.from(tabs).map((skinTab, i) => {
+                        return {
+                            title: skinTab.getAttribute("title"),
+                            image: "https://azurlane.koumakan.jp" + skinTab.getElementsByTagName("img")[0].getAttribute("src"),
+                            chibi: arts[i * 2 + 1].getAttribute("href")
+                        };
+                    }),
+                    buildTime: doc.querySelector("tr:nth-child(1) > td:nth-child(2) > a").textContent,
+                    rarity: cacheShip.rarity,
+                    stars: doc.querySelector("div:nth-child(1) > div:nth-child(3) > .wikitable:nth-child(1) tr:nth-child(2) > td").textContent.trim(),
+                    class: doc.querySelector("div:nth-child(3) > .wikitable tr:nth-child(3) > td:nth-child(2) > a").textContent,
+                    nationality: cacheShip.nationality,
+                    hullType: doc.querySelector(".wikitable tr:nth-child(3) a:nth-child(2)").textContent,
+                    stats: Object.values(doc.querySelectorAll(".tabbertab:nth-child(3) > .wikitable tbody td")).map(cell => cell.textContent.trim()),
+                    author: doc.querySelector(".nomobile:nth-child(1) tr:nth-child(2) a").textContent,
+                };
+                console.log(`Ship Loaded: ${JSON.stringify(ship)}`);
+                SHIPS_CACHE[cacheShip.id] = ship;
+                resolve(ship);
+            });
+        } else {
+            reject(Error("There is no such ship."))
+        };
+    });
+}
+
+exports.getShipByName = getShipByName;
+exports.initiate = function() {
+    return new Promise((resolve, reject) => {
+        request({
+            url: "https://azurlane.koumakan.jp/List_of_Ships",
+            headers: headers
+        }, (error, res, body) => {
+            const doc = new JSDOM(body).window.document;
+            let table_ships = doc.querySelectorAll("#mw-content-text .mw-parser-output table tbody tr");
+            table_ships.forEach(table_ship => {
+                let columns = table_ship.childNodes;
+                SHIPS.push({
+                    id: columns[0].textContent,
+                    name: columns[1].textContent,
+                    rarity: columns[2].textContent,
+                    type: columns[3].textContent,
+                    nationality: columns[4].textContent
+                });
+            });
+            console.log("Loaded " + SHIPS.length + " Ships");
+            resolve(SHIPS);
+        });
+    });
 }
--- test.js
+++ test.js
@@ -1,21 +1,4 @@
-const {
-    AzurLane,
-    Category
-} = require("azurlane");
-const azurlane = new AzurLane();
-
-async function main() {
-    // Get specific ship by name, in this case "Akagi"
-    const ship = await azurlane.getShipByName("Anniversary Kizuna AI");
-    console.log(`${ship.names.en} = ${JSON.stringify(ship)}`); // IJN Akagi's rarity is Super Rare
-    //
-    // // Get an array of ships with rarity "Super Rare"
-    // const ships = await azurlane.getShips(Category.RARITY, "Super Rare");
-    // for (let i = 0; i < ships.length; i++) {
-    //     console.log(`[${ships[i].id}] = ${ships[i].name}`); // [036] = San Diego
-    // }
-}
-
-// Also catches any api errors that might occur like 400, 429, 500 http errors
-// Api errors extend the default error class, have a look at https://kurozeropb.github.io/AzurLane/classes/apierror.html for information
-main().catch(console.error);
+const azurlane = require("./azurlane.js");
+azurlane.initiate().then(ships => azurlane.getShipByName("York")).then(ship => {
+    console.log(JSON.stringify(ship));
+});
