2999f20969c70315f227dd2531b6e0e4f29a0e66
Kumo <h1710169@nushigh.edu.sg>
2019-12-14 11:53:26 +0800

    using azurapi

--- azurlane.js
+++ azurlane.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const config = require('./config.js').config;
-const SHIP_LIST = require('./ship_list.json');
-const SHIPS = require('./ships.json');
+const azurlane = require('@azurapi/azurapi'); // lol i contributed to it you know
 
 const filter = (reaction, user) => ['⬅️', '❎', '➡️'].includes(reaction.emoji.name) && user.id !== config.id;
 const filter2 = (reaction, user) => ['👕'].includes(reaction.emoji.name) && user.id !== config.id;
@@ -22,15 +21,9 @@ const translation = {
     "Anti-submarine warfare": "Anti-Sub"
 }
 const MESSAGES = {};
-exports.ships = SHIPS;
 exports.handleCommnd = async function(args, msg, PREFIX) {
     console.log("running azurlane sub-system...");
     if (args.length < 1) return msg.channel.send("Correct Usage: `" + PREFIX + "azurlane (ship|skin) [args]`");
-    let lang = "en";
-    if (["--en", "--jp", "--cn"].includes(args[args.length - 1])) {
-        console.log("user specified language " + args[args.length - 1]);
-        lang = args.pop().substring(2);
-    }
     switch (args.shift()) {
         case "ship":
         case "s":
@@ -38,8 +31,8 @@ exports.handleCommnd = async function(args, msg, PREFIX) {
         case "i":
             try {
                 console.log("Getting Ship " + args.join(" "));
-                const ship = getShipByName(args.join(" "));
-                let embed = new Discord.RichEmbed().setTitle(`**${ship.names[lang]}**`).setColor(COLOR[ship.rarity]).setThumbnail("https://images.weserv.nl/?url=" + ship.thumbnail).setImage("https://images.weserv.nl/?url=" + ship.skins[0].image).setURL(ship.wikiUrl);
+                const ship = azurlane.getShip(args.join(" "));
+                let embed = new Discord.RichEmbed().setTitle(`**${ship.names.en}** (${ship.names.jp})`).setColor(COLOR[ship.rarity]).setThumbnail(ship.thumbnail).setImage(ship.skins[0].image).setURL(ship.wikiUrl);
                 let stats = ship.stats["Level 120"];
                 embed.addField("**ID**", (ship.id) ? ship.id : "**not yet decided**", true)
                     .addField("**Stars**", ship.stars.stars, true)
@@ -68,74 +61,47 @@ exports.handleCommnd = async function(args, msg, PREFIX) {
         case "skin":
         case "sk":
         case "vs":
-            exeSK(args, msg, lang);
+            const ship = azurlane.getShip(args.join(" "));
+            let skin = ship.skins[0];
+            let embed = new Discord.RichEmbed().setTitle(`**${ship.names[lang]}** (${skin.name})`).setColor(COLOR[ship.rarity]).setThumbnail(skin.chibi).setURL(ship.wikiUrl);
+            embed.addField("Avaliable Skins", ship.skins.map(lskin => lskin.name === skin.name ? "**" + lskin.name + "**" : lskin.name).join("\n"));
+            embed.setImage(skin.image);
+            msg.channel.send(embed).then(message => {
+                if (ship.skins.length > 1) {
+                    message.react('⬅️').then(() => message.react('❎')).then(() => message.react('➡️'));
+                    MESSAGES[message.id] = {
+                        name: ship.names[lang],
+                        skins: ship.skins,
+                        embed: embed,
+                        currentSkin: ship.skins.findIndex(lskin => lskin.name === skin.name),
+                        message: message
+                    };
+                    const collector = message.createReactionCollector(filter);
+                    collector.on('collect', r => {
+                        console.log(`Collected ${r.emoji.name}`);
+                        switch (r.emoji.name) {
+                            case '❎':
+                                r.message.delete();
+                                delete MESSAGES[r.message.id];
+                                break;
+                            case '⬅️':
+                            case '➡️':
+                                message.reactions.forEach(reaction => reaction.users.filter(user => user.id !== config.id).forEach((id, user) => reaction.remove(user)));
+                                let oldSkin = MESSAGES[r.message.id].currentSkin;
+                                MESSAGES[r.message.id].currentSkin = Math.min(Math.max(MESSAGES[r.message.id].currentSkin + (r.emoji.name === '⬅️' ? -1 : 1), 0), MESSAGES[r.message.id].skins.length - 1);
+                                if (oldSkin == MESSAGES[r.message.id].currentSkin) break;
+                                let currentSkin = MESSAGES[r.message.id].skins[MESSAGES[r.message.id].currentSkin];
+                                MESSAGES[r.message.id].embed.fields[0].value = MESSAGES[r.message.id].skins.map(lskin => lskin.name === currentSkin.name ? "**" + lskin.name + "**" : lskin.name).join("\n");
+                                MESSAGES[r.message.id].embed.setTitle(`**${MESSAGES[r.message.id].name}** (${currentSkin.name})`).setThumbnail("https://images.weserv.nl/?url=" + currentSkin.chibi).setImage("https://images.weserv.nl/?url=" + currentSkin.image);
+                                MESSAGES[r.message.id].message.edit(MESSAGES[r.message.id].embed);
+                                break;
+                        }
+                    });
+                    collector.on('end', r => {
+                        message.delete();
+                    });
+                }
+            }); // Pages
             break;
     }
-}
-
-async function exeSK(args, msg, lang) {
-    if (args.length < 1) return msg.channel.send("Correct Usage: `" + PREFIX + "azurlane skin ship-name|skin-name`");
-    try {
-        let newArgs = args.join(" ").split(/ *\| */g);
-        if (newArgs.length == 1) newArgs = [newArgs[0], "Default"];
-        const ship = getShipByName(newArgs[0]);
-        let skin = ship.skins.filter(skin => skin.name.toUpperCase().includes(newArgs[1].toUpperCase()))[0];
-        let embed = new Discord.RichEmbed().setTitle(`**${ship.names[lang]}** (${skin.name})`).setColor(COLOR[ship.rarity]).setThumbnail("https://images.weserv.nl/?url=" + skin.chibi).setURL(ship.wikiUrl);
-        embed.addField("Avaliable Skins", ship.skins.map(lskin => lskin.name === skin.name ? "**" + lskin.name + "**" : lskin.name).join("\n"));
-        embed.setImage("https://images.weserv.nl/?url=" + skin.image);
-        msg.channel.send(embed).then(message => {
-            if (ship.skins.length > 1) {
-                message.react('⬅️').then(() => message.react('❎')).then(() => message.react('➡️'));
-                MESSAGES[message.id] = {
-                    name: ship.names[lang],
-                    skins: ship.skins,
-                    embed: embed,
-                    currentSkin: ship.skins.findIndex(lskin => lskin.name === skin.name),
-                    message: message
-                };
-                const collector = message.createReactionCollector(filter);
-                collector.on('collect', r => {
-                    console.log(`Collected ${r.emoji.name}`);
-                    switch (r.emoji.name) {
-                        case '❎':
-                            r.message.delete();
-                            delete MESSAGES[r.message.id];
-                            break;
-                        case '⬅️':
-                        case '➡️':
-                            message.reactions.forEach(reaction => reaction.users.filter(user => user.id !== config.id).forEach((id, user) => reaction.remove(user)));
-                            let oldSkin = MESSAGES[r.message.id].currentSkin;
-                            MESSAGES[r.message.id].currentSkin = Math.min(Math.max(MESSAGES[r.message.id].currentSkin + (r.emoji.name === '⬅️' ? -1 : 1), 0), MESSAGES[r.message.id].skins.length - 1);
-                            if (oldSkin == MESSAGES[r.message.id].currentSkin) break;
-                            let currentSkin = MESSAGES[r.message.id].skins[MESSAGES[r.message.id].currentSkin];
-                            MESSAGES[r.message.id].embed.fields[0].value = MESSAGES[r.message.id].skins.map(lskin => lskin.name === currentSkin.name ? "**" + lskin.name + "**" : lskin.name).join("\n");
-                            MESSAGES[r.message.id].embed.setTitle(`**${MESSAGES[r.message.id].name}** (${currentSkin.name})`).setThumbnail("https://images.weserv.nl/?url=" + currentSkin.chibi).setImage("https://images.weserv.nl/?url=" + currentSkin.image);
-                            MESSAGES[r.message.id].message.edit(MESSAGES[r.message.id].embed);
-                            break;
-                    }
-                });
-                collector.on('end', r => {
-                    message.delete();
-                });
-            }
-        }); // Pages
-    } catch (err) {
-        console.log(`ship subcommand, err code = ${err.statusCode}, err message = ${err.message}, args = ${args}`);
-        msg.channel.send("Invalid ship name/skin name.");
-    }
-}
-
-function findExactShip(name) {
-    return SHIP_LIST.find(ship => ship.name.toUpperCase() === name.toUpperCase());
-}
-
-function findShip(name) {
-    return SHIP_LIST.find(ship => ship.name.toUpperCase().includes(name.toUpperCase()));
-}
-
-function getShipByName(name) {
-    let cacheShip = findExactShip(name);
-    if (!cacheShip) cacheShip = findShip(name);
-    if (cacheShip) return SHIPS[cacheShip.id];
-    else return null;
-}
+};
