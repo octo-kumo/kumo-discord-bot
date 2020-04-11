ffa15c306468bb2c736cae1d15c83c9eacc1cfb0
Misuti <h1710169@nushigh.edu.sg>
2020-04-11 14:00:14 +0800

    Covid-19

--- covid.js
+++ covid.js
@@ -6,6 +6,8 @@ const fs = require('fs');
 
 let cacheDate = 0;
 let cacheData = null;
+let globalData = null;
+let leaderBoard = null;
 
 let cacheDateGoogle = 0;
 let cacheDataGoogle = null;
@@ -13,12 +15,8 @@ let cacheDataGoogle = null;
 let updatingMessage;
 
 exports.update = async (channel) => {
-    const now = Date.now();
+    await refreshData();
     const region = "singapore";
-    if (now - cacheDate > 60000) { // 1 minute cache
-        cacheDate = now;
-        cacheData = await fetch('https://pomber.github.io/covid19/timeseries.json').then(res => res.json());
-    }
     let found = null;
     let location = null;
     for (let key of Object.keys(cacheData)) {
@@ -35,34 +33,38 @@ exports.update = async (channel) => {
 exports.handleCommand = async (args, msg, PREFIX) => {
     console.log("Running COVID sub-system. args = " + args);
     if (args.length === 0) args = ['SINGAPORE'];
-    const now = Date.now();
-    if (now - cacheDate > 60000) { // 1 minute cache
-        cacheDate = now;
-        cacheData = await fetch('https://pomber.github.io/covid19/timeseries.json').then(res => res.json());
-    }
+    await refreshData();
     const region = args.join(" ").trim().toLowerCase();
-    console.log("COVID: Requested region = " + region);
-    let found = null;
-    let location = null;
-    for (let key of Object.keys(cacheData)) {
-        if (key.toLowerCase() === region) {
-            found = cacheData[key];
-            location = key;
-            break;
-        }
-    }
-    if (!found)
+    if (region === "world" || region === "globe" || region === "global") {
+        msg.channel.send(await generateRegionEmbed("Global", globalData, msg, true));
+    } else {
+        console.log("COVID: Requested region = " + region);
+        let found = null;
+        let location = null;
         for (let key of Object.keys(cacheData)) {
-            if (key.toLowerCase().includes(region)) {
+            if (key.toLowerCase() === region) {
                 found = cacheData[key];
                 location = key;
                 break;
             }
         }
-    msg.channel.send(await generateRegionEmbed(location, found, msg));
+        if (!found)
+            for (let key of Object.keys(cacheData)) {
+                if (key.toLowerCase().includes(region)) {
+                    found = cacheData[key];
+                    location = key;
+                    break;
+                }
+            }
+        msg.channel.send(await generateRegionEmbed(location, found, msg));
+    }
 };
 
-const generateRegionEmbed = async (location, region, msg) => {
+const LONG_NAMES = {
+    "United Kingdom": "UK"
+}
+
+async function generateRegionEmbed(location, region, msg, includeLeaderBoard) {
     const embed = new Discord.RichEmbed();
     embed.setColor(0x0074D9);
     if (msg) embed.setFooter("Query by " + msg.author.tag, msg.author.avatarURL);
@@ -94,6 +96,14 @@ const generateRegionEmbed = async (location, region, msg) => {
             embed.attachFile(new Discord.Attachment(await drawGraph(location, region), "attachment.png"))
             embed.setImage("attachment://attachment.png")
             if (!msg) embed.setDescription("_This message is automatically updated every 1 hour_");
+            if (includeLeaderBoard) {
+                let desc = [];
+                desc.push("#  " + "Region".padEnd(7, " ") + " " + "Cases".padStart(6, " ") + " " + "Dead".padStart(6, " ") + " " + "Heal".padStart(6, " "))
+                for (let i = 0; i < 8; i++) {
+                    desc.push(`#${(i+1)} ${(LONG_NAMES[leaderBoard[i].region]?LONG_NAMES[leaderBoard[i].region]:leaderBoard[i].region).padEnd(7," ")} ${formatNumber(leaderBoard[i].confirmed).padStart(6," ")} ${formatNumber(leaderBoard[i].deaths).padStart(6," ")} ${formatNumber(leaderBoard[i].recovered).padStart(6," ")}`);
+                }
+                embed.setDescription("```" + desc.join("\n") + "```");
+            }
         } else {
             embed.setTitle("Waiting for Data")
                 .setDescription("Newest data is yet to be released");
@@ -155,4 +165,83 @@ function convertToData(region) {
         });
     }
     return newDataArray;
+}
+
+async function refreshData() {
+    const now = Date.now();
+    if (now - cacheDate > 60000) { // 1 minute cache
+        cacheDate = now;
+        cacheData = await fetch('https://pomber.github.io/covid19/timeseries.json').then(res => res.json());
+        globalData = combineData();
+        leaderBoard = compileLeaderboard();
+    }
+}
+
+function combineData() {
+    const global_data = {}; // days mapped by date
+    for (let key of Object.keys(cacheData)) {
+        for (let day of cacheData[key]) {
+            if (!global_data[day.date]) {
+                global_data[day.date] = {
+                    confirmed: 0,
+                    deaths: 0,
+                    recovered: 0
+                };
+            }
+            global_data[day.date].confirmed += day.confirmed;
+            global_data[day.date].deaths += day.deaths;
+            global_data[day.date].recovered += day.recovered;
+        }
+    }
+    const global_array = [];
+    for (let date of Object.keys(global_data)) {
+        let day = global_data[date];
+        global_array.push({
+            date: date,
+            confirmed: day.confirmed,
+            deaths: day.deaths,
+            recovered: day.recovered
+        });
+    }
+    return global_array;
+}
+
+function compileLeaderboard() {
+    let leaderBoard = [];
+    for (let key of Object.keys(cacheData)) {
+        let today = cacheData[key][cacheData[key].length - 1];
+        leaderBoard.push({
+            region: key,
+            confirmed: today.confirmed,
+            deaths: today.deaths,
+            recovered: today.recovered
+        });
+    }
+    leaderBoard.sort((a, b) => {
+        if (a.confirmed < b.confirmed) return 1;
+        else if (a.confirmed == b.confirmed) return 0;
+        else return -1;
+    });
+    leaderBoard = leaderBoard.slice(0, 8);
+    return leaderBoard;
+}
+
+function findWithDate(region, date) {
+    for (let day of region) {
+        if (day.date === date) return day;
+    }
+    return null;
+}
+
+const UNITS = ['k', 'm', 'b', 't', 'q', 'Q'];
+
+function formatNumber(number) {
+    if (number < 1000) return number.toString();
+    let counter = 0;
+    let base = 1000;
+    while (number > 1000 * base) {
+        base *= 1000;
+        counter++;
+    }
+    return Number.parseFloat(number / base).toPrecision(4) + UNITS[counter];
 }
