d297940b1e1d3628396c2877dc6b7b207c3a1b46
octo-kumo <h1710169@nushigh.edu.sg>
2020-10-02 23:01:06 +0800

    i died

--- commands/eco.js
+++ commands/eco.js
@@ -3,7 +3,7 @@ const db = require("../db");
 
 const round = (n, p) => parseFloat(String(n)).toFixed(p);
 exports.flip_coin = function (args, msg, PREFIX) {
-    db.User.findOrCreate({id: msg.author.id}).then(user => {
+    db.User.findOrCreate({id: msg.author.id}, function (err, user) {
         if (user.credit < 50) return msg.reply("A flip coin takes **$50**, and you don't have enough");
         if (Math.random() > 0.5) {
             user.credit += 50;
@@ -19,7 +19,7 @@ exports.flip_coin = function (args, msg, PREFIX) {
 exports.balance = function (args, msg, PREFIX) {
     if (msg.mentions.users.size > 0) db.User.findOne({id: msg.mentions.users.first().id}).then(user => {
         msg.reply(msg.mentions.users.first().username + " has **$" + (user.credit || 0) + "**");
-    }); else db.User.findOrCreate({id: msg.author.id}).then(user => {
+    }); else db.User.findOrCreate({id: msg.author.id}, function (err, user) {
         msg.reply("You have **$" + (user.credit || 0) + "**");
     });
 }
@@ -35,7 +35,7 @@ exports.daily = function (args, msg, PREFIX) {
         user.save();
     });
     if (args[0] === "count") {
-        return db.User.findOrCreate({id: msg.author.id}).then(user => {
+        return db.User.findOrCreate({id: msg.author.id}, function (err, user) {
             msg.reply("You have claimed **daily** " + user.daily_count + " times.");
         });
     } else if (msg.mentions.users.size > 0) {
@@ -43,7 +43,7 @@ exports.daily = function (args, msg, PREFIX) {
             msg.reply(msg.mentions.users.first().username + " has claimed **daily** " + user.daily_count + " times.");
         });
     } else {
-        return db.User.findOrCreate({id: msg.author.id}).then(user => {
+        return db.User.findOrCreate({id: msg.author.id}, function (err, user) {
             let diff = Date.now() - (user.daily_last_claimed || 0);
             if (diff < 86400000) return msg.reply("Please wait for `" + Math.floor((86400000 - diff) / 1000) + "s` before claiming daily again!");
             user.daily_count = (user.daily_count || 0) + 1;
