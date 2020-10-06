bbf10d7e0f8af9ff2011fa5bd552f4a2914154ca
octo-kumo <h1710169@nushigh.edu.sg>
2020-10-06 09:44:28 +0800

    ok

--- commands/bilibili.js
+++ commands/bilibili.js
@@ -5,10 +5,8 @@ exports.init = function () {
     return fetch("https://www.bilibili.com/activity/web/view/data/31").then(r => r.json()).then(json => DEFAULT_PICS = json.data.list);
 }
 exports.handleCommand = async function (args, msg, PREFIX) {
-    if (args.length === 0) {
-        let image = DEFAULT_PICS[Math.floor(DEFAULT_PICS.length * Math.random())];
-        let url = image.data.img;
-        if (!url.startsWith("http")) url = "https:" + url;
-        await msg.channel.send(new Discord.RichEmbed().setColor(0x44a0d1).setImage(url));
-    }
+    let image = DEFAULT_PICS[Math.floor(DEFAULT_PICS.length * Math.random())];
+    let url = image.data.img;
+    if (!url.startsWith("http")) url = "https:" + url;
+    await msg.channel.send(new Discord.RichEmbed().setColor(0x44a0d1).setImage(url));
 }
