b194f6ba5b7cfc7c677e83b59fdd9ad8418b972b
octo-kumo <h1710169@nushigh.edu.sg>
2021-01-31 12:26:31 +0900

    weird

--- commands/xkcd.js
+++ commands/xkcd.js
@@ -5,12 +5,13 @@ exports.handleCommand = async function (args, msg, PREFIX) {
     let doc = new JSDOM(await fetch(args.length === 0 ? "https://c.xkcd.com/random/comic/" : "https://xkcd.com/" + args[0] + "/").then(res => res.text())).window.document;
     try {
         let url = doc.head.querySelector("[property='og:url'][content]").content;
+        let img = doc.querySelector('#comic img');
         await msg.channel.send(new Discord.MessageEmbed()
             .setColor(0x44a0d1)
             .setURL(url)
             .setTitle(doc.head.querySelector("[property='og:title'][content]").content)
-            .setImage(doc.head.querySelector("[property='og:image'][content]").content)
-            .setDescription(doc.querySelector('#comic img').getAttribute('title'))
+            .setImage('https:' + (img.getAttribute('srcset') ? img.getAttribute('srcset') : img.getAttribute('src')))
+            .setDescription(img.getAttribute('title'))
             .setFooter('All credits to https://xkcd.com • ' + url.replace(/^https:\/\/xkcd.com\/([^/]+)\/$/, '$1'))
         );
     } catch (e) {
